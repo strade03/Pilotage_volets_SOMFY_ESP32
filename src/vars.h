5df46a98d6684c03ccef5cb36521fa1b4f956f2f
StephV <97481110+strade03@users.noreply.github.com>
2024-02-01 20:22:38 +0100

    vars.h

--- src/vars.h
+++ src/vars.h
@@ -1,1 +1,25 @@
+// Exemple de fichier vars.h 
+#ifndef VARS_H_
+#define VARS_H_
 
+// #define delay_after_move // Si actif fait une pause entre les programmes
+
+#define REMOTES_COUNT 10
+
+const char *remote_name[REMOTES_COUNT] = { 
+  (char *)"Cuisine", 
+  (char *)"Baie vitr&eacute;e",
+  (char *)"Buffet",
+  (char *)"Porte fen&ecirc;tre",
+  (char *)"Bureau",
+  (char *)"Chambre 1",
+  (char *)"Chambre 2",
+  (char *)"Chambre 3",
+  (char *)"Tout bas",
+  (char *)"Tout haut",
+};
+
+// Ordre d'affichage des télécommandes permettant d'en ajouter facilement 
+const int remote_order[REMOTES_COUNT] = { 0,1,2,5,6,3,4,7,8,9 };
+
+#endif /* VARS_H_ */
