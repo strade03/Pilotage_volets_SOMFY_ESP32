d5c3ca5400cd59aa2234768e9697fe0f673fd7a8
StephV <verdier.st@gmail.com>
2024-02-01 20:15:03 +0100

    modified:   .gitignore

--- src/vars.h
+++ src/vars.h
@@ -22,4 +22,4 @@ const char *remote_name[REMOTES_COUNT] = {
 // Ordre d'affichage des télécommandes permettant d'en ajouter facilement 
 const int remote_order[REMOTES_COUNT] = { 0,1,2,5,6,3,4,7,8,9 };
 
-#endif /* VARS_H_ */
+#endif /* VARS_H_ */ 
